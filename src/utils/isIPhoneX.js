ec9a9b059abc0ce6494fdd05ae56bc8ccaad3350
Hugh Molotsi <bitbucket@hughie.com>
2019-03-23 17:32:31 -0700

    Support iPhone XR and XS Max dimensions.

--- src/utils/isIPhoneX.js
+++ src/utils/isIPhoneX.js
@@ -1,4 +1,4 @@
 import { Platform, Dimensions } from 'react-native';
 const { width, height } = Dimensions.get('window');
-const isIPhoneX = Platform.OS === 'ios' && width === 375 && height === 812;
+const isIPhoneX = Platform.OS === 'ios' && ((width === 375 && height === 812) || (width === 414 && height === 896));
 export default isIPhoneX;
